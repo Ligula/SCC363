5b4af55afbaf101c4e57aeab31e344f648e9ad0f
James <James@T450S.com>
2020-01-21 15:28:42 +0000

    Added some basic endpoints for login and one-time code.

--- simpleServer.py
+++ simpleServer.py
@@ -11,6 +11,23 @@ def hello_world():
     return 'Hello World'
 
 
+@app.route('/api/v1/login', methods=['POST'])
+def login_handler():
+    # Check password is correct
+    # Provide the user with a one time code via email.
+    # Send user to code entry form.
+    return "Login handler"
+
+@app.route('/api/v1/otc', method=['POST'])
+def otc_handler():
+    # One time code handler
+    # Check OTC matches the stored OTC
+    return "One time code"
+
+@app.route('/api/v1/register', methods=['POST'])
+def register_handler():
+    return "Register handler"
+
 # This account doesn't actually exist yet.
 email_user = "scc363-verify@gmail.com"
 email_password = "some_magic_password"
