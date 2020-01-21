8a852f87df9bfc4284a753b2b9b0c65f2f609cac
Alexander <alexander.e.cole@gmail.com>
2020-01-21 14:34:51 +0000

    simple server

---
+++ simpleServer.py
@@ -0,0 +1,19 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import ssl
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'Hello, world!')
+
+
+
+httpd = HTTPServer(('localhost', 8000), BaseHTTPRequestHandler)
+
+#httpd.socket = ssl.wrap_socket (httpd.socket, 
+#        keyfile="path/to/key.pem", 
+#        certfile='path/to/cert.pem', server_side=True)
+
+httpd.serve_forever()
