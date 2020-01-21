3d23fc2a94c50616b31ce7cd1ff692d1b42fb8eb
Paul <paulalcock656@tutanota.com>
2020-01-21 15:26:32 +0000

    using hashlib to hash password

--- client.py
+++ client.py
@@ -1,7 +1,24 @@
-import requests
+import requests, json, hashlib, sys
+
+#Steps:
+#
 
 
-login = raw_input('Login: ')
-password = raw_input('Password; ')
+login = input('Login: ')
+password = input('Password: ')
+
+
+data = {
+  "username": login,
+  "password": password
+}
+
+hashword = hashlib.sha3_256(password.encode('utf-8')).hexdigest()
+print(hashword)
+
+jsonData = json.dumps(data)
+
+
+
 
 print("Well done!")
