f64787ac73ce6337c2539606eccb7bc0808d30d7
Guilvareux <paulalcock656@tutanota.com>
2020-01-24 00:45:08 +0000

    simple responses

--- simpleServer.py
+++ simpleServer.py
@@ -1,16 +1,22 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
-from flask import Flask
-import ssl, requests
+from flask import Flask, request, Response
+import ssl
 
 context = ('certificate.pem', 'key.pem')
 
 app = Flask(__name__)
 
-users = dict()
+users = {
+    "testUser": "testPassword"
+}
 
 @app.route('/')
 def hello_world():
     return 'Hello World'
+
+@app.route('/alive')
+def alive():
+    return 'Alive'
 
 
 @app.route('/api/v1/login', methods=['POST'])
@@ -23,12 +29,12 @@ def login_handler():
         # Check password is valid
         # Hash password on the server
         # If the password is correct, send one-time code and store one-time code for testing
-        return {'message': 'You have logged in'}, status.HTTP_200_OK
+        return Response("{'message': 'You have logged in'}", status=200)
     else:
-        return {'message' : "User doesn't exists"}, status.HTTP_404_NOT_FOUND
+        return Response("{'message' : 'User doesn't exists'}", status=404)
     return "Login handler"
 
-@app.route('/api/v1/otc', method=['POST'])
+@app.route('/api/v1/otc', methods=['POST'])
 def otc_handler():
     data = request.get_json()
     # One time code handler
@@ -38,6 +44,10 @@ def otc_handler():
 @app.route('/api/v1/register', methods=['POST'])
 def register_handler():
     data = request.get_json()
+    email = data["email"]
+    uname = data["username"]
+    pwd = data["password"]
+
     return "Register handler"
 
 # This account doesn't actually exist yet.
@@ -65,7 +75,7 @@ def SendEmail(email, subject, body):
         server.close()
         return True
     except:
-        print 'Something went wrong...'
+        print('Something went wrong...')
         return False
 
 if __name__ == '__main__':
