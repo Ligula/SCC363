c19cff97720eecd8e0ac0fcc698fac3b597c7e3c
James <James@T450S.com>
2020-02-06 19:17:48 +0000

    Basic auditor works, just need to implement in server.

--- auditor.py
+++ auditor.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import os
 import pickle
+from threading import Lock
 
 class AuditEntry(object):
     def __init__(self, time, operation, userId, ipAddr, reason):
@@ -10,33 +11,50 @@ class AuditEntry(object):
         self.ipAddr = ipAddr
         self.reason = reason
 
+    def toDict(self):
+        entry = {}
+        entry["time"] = self.time
+        entry["op"] = self.operation
+        entry["uid"] = self.userId
+        entry["remote_ip"] = self.ipAddr
+        entry["reason"] = self.reason
+        return entry
+
 class Auditor:
     def __init__(self, auditFile):
         self.file = auditFile
         # Create file if not exists.
         self.handle = open("audit.log", 'a+')
         self.handle.close()
+        # TODO: some exclusive lock for the file to stop other processes from reading it.
         self.handle = open("audit.log", 'r+b')
+        self.mutex = Lock()
 
     def pushEvent(self, operation, userId, ipAddr, reason):
         eventTime = datetime.now()
+        self.mutex.acquire()
         entry = AuditEntry(eventTime, operation, userId, ipAddr, reason)
         self.handle.seek(0, os.SEEK_END)
         pickle.dump(entry, self.handle)
+        self.mutex.release()
 
     def readLogs(self):
-        # Read all logs and parse into JSON object.
+        # Read all logs and parse into dictionary object.
+        self.mutex.acquire()
         self.handle.seek(0, os.SEEK_SET)
         logs = []
         while 1:
             try:
-                logs.append(pickle.load(self.handle))
+                e = pickle.load(self.handle)
+                # Convert from object to dictionary
+                logs.append(e.toDict())
             except (EOFError, pickle.UnpicklingError):
-                break        
+                break
+        self.mutex.release()
         return logs
 
-
 if __name__ == "__main__":
+    # Some test code...
     auditor = Auditor("audit.log")
     auditor.pushEvent("Logging in", "james", "192.168.1.1", "accessing website")
     print(auditor.readLogs())
